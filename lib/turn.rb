77250a6b12795fd3e9ecc7d09bed3d6a8849b7a1
Adam Boyajian <adam.boyajian@wework.com>
2018-02-16 01:22:34 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,6 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board,index)
     display_board(board)
-  else
     until valid_move?(board,index) == true 
     turn(board)
     end
