ab3d34d7f881355a21d0c53af5d5f649c194e211
Adam Boyajian <adam.boyajian@wework.com>
2018-02-16 00:11:06 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,8 +8,6 @@ def display_board(board)
   puts " #{board[6]} " "|" " #{board[7]} " "|" " #{board[8]} "
 end
 
-user_input = between(1-9)
-
 def input_to_index(user_input)
   converted_input = user_input.to_i - 1 
 end
