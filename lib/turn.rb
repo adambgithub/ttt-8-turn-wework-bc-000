8277b1e56321d70cd43c4e9c2e408e143a51b0c9
Adam Boyajian <adam.boyajian@wework.com>
2018-02-16 01:44:46 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,8 +40,8 @@ def turn(board)
   index = input_to_index(user_input)
   converted_input = user_input.to_i - 1
   if valid_move?(board,index)
-    display_board(board)
     move(board, converted_input, value = "X")
+    display_board(board)
   else 
     turn(board)
   end
